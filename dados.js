92d127e9605bb447ca855f6163355647712108e6
Andrei Gravonski <gravonskia@gmail.com>
2024-09-08 11:49:46 -0300

    Update dados

--- dados.js
+++ dados.js
@@ -16,6 +16,48 @@ let dados = [
         local: "Cairu – Bahia",
         descricao: "Localizada na Ilha de Tinharé, a Praia de Garapuá é conhecida por suas águas cristalinas e areia branca. É um dos principais destinos turísticos da região, oferecendo opções de hospedagem, restaurantes e atividades náuticas.",
         link: "https://www.melhoresdestinos.com.br/melhores-praias-do-brasil.html"
-      } //Indíce 2//
+      }, //Indíce 2//
+      {
+        nome: "Praia Brava",
+        local: "Buzios – Rio de Janeiro",
+        descricao: "A bela paisagem do mirante na estrada que leva à Brava já seria motivo suficiente para atrair os turistas até lá, mas ela vai muito além e oferece um mar maravilhoso, ambiente sempre tranquilo, faixa de areia vazia e dois dos mais elegantes bares à beira-mar da cidade.",
+        link: "https://www.melhoresdestinos.com.br/melhores-praias-do-brasil.html"
+      },
+      {
+        nome: "Praia da Baía dos Golfinhos",
+        local: "Pipa - Rio Grande do Norte",
+        descricao: "Pipa, no Rio Grande do Norte, é aqueles destinos que agradam todos os públicos: desde os que buscam badalação até os que estão mais dispostos a curtir a tranquilidade da vida à beira-mar. Por lá, os visitantes são convidados a passar o dia nas diversas faixas de areia, fazer passeios de jardineira, buggy ou barco, curtir o pôr do sol na Lagoa Guaraíras e a noite no badalado centrinho.",
+        link: "https://www.melhoresdestinos.com.br/melhores-praias-do-brasil.html"
+      },
+      {
+        nome: "Praia da Baía dos Porcos",
+        local: "Fernando de Noronha - Pernambuco",
+        descricao: "Pequena gigante, assim é a linda Baía dos Porcos. Uma praia formada no meio do paredão de pedras, cercada por verde e por piscinas naturais acessíveis durante a maré baixa, o que torna o lugar ótimo para a prática de snorkel (com algumas restrições de preservação ambiental). A Baía dos Porcos, considerada uma das praias mais bonitas do Brasil, tem águas esverdeadas que costumam ser visitadas também por tartarugas, o que dá ainda mais charme ao lugar.",
+        link: "https://www.melhoresdestinos.com.br/melhores-praias-do-brasil.html"
+      },
+      {
+        nome: "Praia da Ilha do Campeche",
+        local: "Florianópolis - Santa Catarina",
+        descricao: "A ilha tem praia de águas cristalinas, tranquilas e ideais para relaxar. Além de ser conhecida por sua beleza, é também um patrimônio arqueológico, já que possui inscrições rupestres de grande valor. Para conhecer a ilha, é preciso ir até a Praia da Armação ou à Praia do Campeche, onde se pega uma pequena embarcação que faz a travessia até o local. É um passeio lindo!",
+        link: "https://www.melhoresdestinos.com.br/melhores-praias-do-brasil.html"
+      },
+      {
+        nome: "Praia da Ilha do Prumirim",
+        local: "Ubatuba - São Paulo",
+        descricao: "A Ilha do Prumirim, em Ubatuba, não perde em nada em beleza para a vizinha mais famosa, a Ilha das Couves, uma das praias mais bonitas do Brasil Com mar transparente, longa faixa de areia branquinha, fácil acesso e pedras que formam belos cenários para fotos, a Ilha do Prumirim é visita imperdível para quem gosta de porções de areia à beira-mar.",
+        link: "https://www.melhoresdestinos.com.br/melhores-praias-do-brasil.html"
+      },
+      {
+        nome: "Praia da Lagoa do Paraíso",
+        local: "Jericoacoara – Ceará",
+        descricao: "A Lagoa do Paraíso, em Jericoacoara, é uma das mais belas da região. Com água entre diversos tons de azul, a Lagoa do Paraíso proporciona um grande espetáculo para os olhos e encanta a quem escolhe o lugar para descansar. As redes dentro da água já são tradição, mas por lá há também vários restaurantes, que colocam mesas e cadeiras dentro da água para que os visitantes relaxarem ainda mais.",
+        link: "https://www.melhoresdestinos.com.br/melhores-praias-do-brasil.html"
+      },
+      {
+        nome: "Quarta Praia",
+        local: "Morro de São Paulo – Bahia",
+        descricao: "A Quarta Praia de Morro de São Paulo é o lugar certo para ficar longe do movimento e curtir o lindo cenário natural da ilha. Com 4 km de faixa de areia e permeada com belas formações de piscinas naturais, a Quarta Praia é um convite a se isolar e relaxar. A infraestrutura da Quarta Praia está limitada ao início da faixa de areia (próximo à Terceira Praia), onde estão as piscinas naturais que aparecem na maré baixa e rendem até um bom mergulho de superfície, com direto a ver peixinhos.",
+        link: "https://www.melhoresdestinos.com.br/melhores-praias-do-brasil.html"
+      }
 ]; 
 
